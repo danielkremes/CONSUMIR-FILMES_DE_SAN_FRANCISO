826f998e5da077c340fa24ae503e28737f6b58de
danielkremes <danielkremes06@gmail.com>
2025-07-06 17:23:48 -0300

    feat: finished methods in service

---
+++ src/main/java/br/san/franciso/api_movies_san_francisco/service/MovieLocationService.java
@@ -0,0 +1,29 @@
+package br.san.franciso.api_movies_san_francisco.service;
+
+import br.san.franciso.api_movies_san_francisco.model.MovieLocation;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class MovieLocationService {
+
+    private final WebClient client;
+
+    public MovieLocationService(WebClient.Builder client) {
+        this.client = client.baseUrl("https://data.sfgov.org/resource/yitu-d5am.json").build();
+    }
+
+    public List<MovieLocation> listMovies() {
+        return client.get().retrieve().bodyToFlux(MovieLocation.class).collectList().block();
+    }
+
+    // Pagination by Title
+    public List<MovieLocation> filterByTitle(String query) {
+        return listMovies().stream().filter(
+                m -> m.getTitle() != null && m.getTitle().toLowerCase().contains(query.toLowerCase())
+        ).collect(Collectors.toList());
+    }
+}
