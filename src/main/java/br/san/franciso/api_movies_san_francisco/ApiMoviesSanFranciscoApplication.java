a5da869f91346ffd27381686bf3d943cc5449d25
danielkremes <danielkremes06@gmail.com>
2025-07-06 16:39:26 -0300

    chore: create project initial

---
+++ src/main/java/br/san/franciso/api_movies_san_francisco/ApiMoviesSanFranciscoApplication.java
@@ -0,0 +1,13 @@
+package br.san.franciso.api_movies_san_francisco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiMoviesSanFranciscoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiMoviesSanFranciscoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/san/franciso/api_movies_san_francisco/ApiMoviesSanFranciscoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.san.franciso.api_movies_san_francisco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiMoviesSanFranciscoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
